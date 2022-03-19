6f090a18d33e808310939da7835f264ac9860274
Laufer <eaparova@gmail.com>
2022-03-19 22:06:15 +0300

    Замена кнопок изображениями

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.math.floor
@@ -17,8 +18,8 @@ class MainActivity : AppCompatActivity() {
     перед попыткой использоать содержимое свойства*/
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var backButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var backButton: ImageButton
     private lateinit var questionTextView: TextView
 
     //Создаем базу вопросов с ответами
